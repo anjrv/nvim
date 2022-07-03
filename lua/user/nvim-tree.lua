04d8757a2a67d2a478af0c0d0aa9754a39326d31
Anna Jaerving <AnnaJrving@gmail.com>
2022-07-03 14:36:45 +0000

    tree changes

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -11,13 +11,52 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
+	hijack_directories = {
+		enable = true, -- Open tree when opening vim on a directory
+	},
+	ignore_ft_on_setup = {
+		"startify",
+		"dashboard",
+		"alpha",
+	},
+	filters = {
+		custom = { ".git" },
+		exclude = { ".gitignore" },
+	},
+	-- auto_close = true,
+	-- open_on_tab = false,
+	-- hijack_cursor = false,
+	update_cwd = true,
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
+		ignore_list = {},
 	},
 	renderer = {
+		add_trailing = false,
+		group_empty = false,
+		highlight_git = false,
+		highlight_opened_files = "none",
 		root_folder_modifier = ":t",
+		indent_markers = {
+			enable = false,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				none = "  ",
+			},
+		},
 		icons = {
+			webdev_colors = true,
+			git_placement = "before",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
 			glyphs = {
 				default = "",
 				symlink = "",
@@ -53,7 +92,15 @@ nvim_tree.setup({
 			error = "",
 		},
 	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
 	view = {
+		hide_root_folder = false,
+		number = false,
+		relativenumber = false,
 		width = 30,
 		height = 30,
 		side = "left",
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -5,21 +5,37 @@ end
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		disable = { "css", "markdown" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	autotag = {
+		enable = true,
+		disable = { "xml" },
+	},
 	rainbow = {
 		enable = true,
-		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-		max_file_lines = nil, -- Do not enable for files with more than n lines, int
-		-- colors = {}, -- table of hex strings
-		-- termcolors = {} -- table of colour name strings
+		colors = {
+			"Gold",
+			"Orchid",
+			"DodgerBlue",
+			-- "Cornsilk",
+			-- "Salmon",
+			-- "LawnGreen",
+		},
+		disable = { "html" },
+	},
+	playground = {
+		enable = true,
 	},
 })
