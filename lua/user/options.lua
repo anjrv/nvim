e0bf26d5c073f6b9f2f5e6b05612652b7f1f90eb
Anna Jaerving <AnnaJrving@gmail.com>
2022-07-31 13:30:23 +0000

    bunch of oopsie fixes

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,7 +34,7 @@ vim.opt.wrap = false -- display lines as one long line
 vim.opt.scrolloff = 8 -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.relativenumber = true
-vim.opt.guifont = "monospace:h17" -- the font used in graphical neovim applications
+vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h16" -- the font used in graphical neovim applications
 vim.opt.fillchars.eob = " "
 vim.opt.shortmess:append("c")
 vim.opt.whichwrap:append("<,>,[,],h,l")
