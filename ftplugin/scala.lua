8b4c187fb7775f2af44994300ba2abf5477e8a0f
Anna Jaerving <AnnaJrving@gmail.com>
2022-12-03 12:12:48 +0000

    metals fix

--- ftplugin/scala.lua
+++ ftplugin/scala.lua
@@ -4,11 +4,11 @@ if not config then
 end
 
 config.settings = {
-	capabilities = require("user.lsp.handlers").capabilities,
 	showImplicitArguments = true,
 	-- excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
+config.capabilities = require("user.lsp.handlers").capabilities
 config.on_attach = require("user.lsp.handlers").on_attach
 
 require("metals").initialize_or_attach(config)
