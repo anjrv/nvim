9954b03cb2d7686680ba937400bda306918fa3e3
Anna Jaerving <AnnaJrving@gmail.com>
2024-04-13 12:43:16 +0000

    reduce lsp message spam

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -91,6 +91,24 @@ require("lazy").setup({
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
+		opts = {
+			lsp = {
+				progress = { enabled = false },
+				message = { enabled = false },
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+		},
 	},
 
 	{
