fd6a404f6710415d7a473a4f84f87ff6c5eb7a9b
Anna Jaerving <AnnaJrving@gmail.com>
2024-05-25 14:27:31 +0000

    try use mini instead of notify

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -89,9 +89,10 @@ require("lazy").setup({
 			-- OPTIONAL:
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			-- "rcarriga/nvim-notify",
 		},
 		opts = {
+			notify = { enabled = false },
 			lsp = {
 				progress = { enabled = false },
 				message = { enabled = false },
