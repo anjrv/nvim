fa8dc098a4d54b2a1acbefd87a73c2e8b077a01e
Anna Jaerving <AnnaJrving@gmail.com>
2025-12-02 20:08:07 +0000

    zen

--- init.lua
+++ init.lua
@@ -389,13 +389,81 @@ require("lazy").setup({
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
+		keys = {
+			{
+				"<leader>bz",
+				function()
+					Snacks.zen()
+				end,
+				desc = "Toggle Zen Mode",
+				mode = "n",
+			},
+		},
 		---@type snacks.Config
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
 			bigfile = { enabled = true },
-			dashboard = { enabled = true },
+			dashboard = {
+				preset = {
+					pick = nil,
+					---@type snacks.dashboard.Item[]
+					keys = {
+						{
+							icon = " ",
+							key = "f",
+							desc = "Find File",
+							action = ":lua Snacks.dashboard.pick('files')",
+						},
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{
+							icon = " ",
+							key = "g",
+							desc = "Find Text",
+							action = ":lua Snacks.dashboard.pick('live_grep')",
+						},
+						{
+							icon = " ",
+							key = "r",
+							desc = "Recent Files",
+							action = ":lua Snacks.dashboard.pick('oldfiles')",
+						},
+						{
+							icon = " ",
+							key = "c",
+							desc = "Config",
+							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+						},
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{
+							icon = "󰒲 ",
+							key = "l",
+							desc = "Lazy",
+							action = ":Lazy",
+							enabled = package.loaded.lazy ~= nil,
+						},
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+					header = [[
+                                                      
+               ████ ██████           █████      ██
+              ███████████             █████ 
+              █████████ ███████████████████ ███   ███████████
+             █████████  ███    █████████████ █████ ██████████████
+            █████████ ██████████ █████████ █████ █████ ████ █████
+          ███████████ ███    ███ █████████ █████ █████ ████ █████
+         ██████  █████████████████████ ████ █████ █████ ████ ██████
+      ]],
+				},
+				sections = {
+					{ section = "header" },
+					{
+						section = "keys",
+						indent = 1,
+						padding = 1,
+					},
+					{ section = "recent_files", icon = " ", title = "Recent Files", indent = 3, padding = 2 },
+					{ section = "startup" },
+				},
+			},
 			explorer = { enabled = false },
 			indent = { enabled = true },
 			input = { enabled = true },
@@ -406,6 +474,19 @@ require("lazy").setup({
 			scroll = { enabled = true },
 			statuscolumn = { enabled = true },
 			words = { enabled = true },
+			zen = {
+				enabled = true,
+				toggles = {
+					ufo = true,
+					dim = true,
+					git_signs = false,
+					diagnostics = false,
+					line_number = false,
+					relative_number = false,
+					signcolumn = "no",
+					indent = false,
+				},
+			},
 		},
 	},
 
